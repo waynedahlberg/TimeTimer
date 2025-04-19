1619e6b3ffca50513c485fa852745bdd00a676d0
Wayne Dahlberg <waynefj40@gmail.com>
2025-04-19 16:30:54 -0600

    Initial Commit

---
+++ TimeTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeTimer
+//
+//  Created by Wayne Dahlberg on 4/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeTimer/TimeTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTimerApp.swift
+//  TimeTimer
+//
+//  Created by Wayne Dahlberg on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
